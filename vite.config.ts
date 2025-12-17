2f3eeaebb058ac43800adf20173b3502088f6c0a
Adilson <afs.adilson@gmail.com>
2025-12-17 07:25:55 -0300

    versao 2.1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/gdx-landing-page/',
+      base: '/',
       publicDir: 'public',
       server: {
         port: 3000,
